2d4904764c60905fd525ab99ba0e1b6c51399aaf
Hector <hca0004@alu.ubu.es>
2018-11-22 12:14:24 +0100

    Interface Datos

--- src/kanban/CSV.java
+++ src/kanban/CSV.java
@@ -5,12 +5,84 @@ import java.util.*;
 /**
  * 
  */
-public class CSV extends Datos {
+public class CSV implements Datos {
 
     /**
      * Default constructor
      */
     public CSV() {
     }
+
+	@Override
+	public ProductBacklog selectProductBacklog() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateProductBacklog(ProductBacklog productBacklog) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public SprintBacklog selectSprintBacklogActual() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateSprintBacklogActual(SprintBacklog sprintBacklog) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<SprintBacklog> selectSprintBacklog() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SprintBacklog selectSprintBacklog(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MiembroDeEquipo insertUsuario(MiembroDeEquipo miembroDeEquipo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MiembroDeEquipo> selectMiembrosDeEquipo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Requisito> selectRequisitos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertRequisito(Requisito requisito) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateTarea() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Datos getInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/kanban/Datos.java
+++ src/kanban/Datos.java
@@ -5,12 +5,27 @@ import java.util.*;
 /**
  * 
  */
-public class Datos {
+public interface Datos {
 
     /**
      * Default constructor
      */
-    public Datos() {
-    }
+	public Datos getInstance();
+	
+    public ProductBacklog selectProductBacklog();
+    public boolean updateProductBacklog(ProductBacklog productBacklog);
+    
+    public SprintBacklog selectSprintBacklogActual();
+    public boolean updateSprintBacklogActual(SprintBacklog sprintBacklog);
+    
+    public List<SprintBacklog> selectSprintBacklog();
+    public SprintBacklog selectSprintBacklog(int id);
+    
+    public MiembroDeEquipo insertUsuario(MiembroDeEquipo miembroDeEquipo);
+    public List<MiembroDeEquipo> selectMiembrosDeEquipo();
 
+    public List<Requisito> selectRequisitos();
+    public boolean insertRequisito(Requisito requisito);
+    
+    public boolean updateTarea();
 }
